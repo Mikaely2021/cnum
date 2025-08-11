f5545f996cabbfd3569f9b9a6d58b5c71256e584
mikaelyalmeida05 <mikaelyalmeida05@gmail.com>
2025-08-11 17:04:40 +0000

    Semana 1

--- .idx/1/mayn.py
+++ .idx/1/mayn.py
@@ -1,12 +1,11 @@
 from math import sqrt
 
 def zeros(a, b, c):
-
     if a == 0:
         if b == 0:
             raise ValueError("A equação não é de segundo grau") 
-        else:
-            return [-c/b]   
+    else:
+        return [-c/b]   
         
     delta = b**2 - (4*a*c)
 
